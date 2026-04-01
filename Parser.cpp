feef6d5f892e11efd71956500412a3b0b084f655
Ghost <ismaelkelvyn328@gmail.com>
2026-04-01 12:08:09 -0300

    Create Parser.cpp

---
+++ Parser.cpp
@@ -0,0 +1,10 @@
+#include "Parser.h"
+
+Node* Parser::parseHTML(const std::string& html) {
+    // Placeholder: futuramente você vai implementar parsing real
+    Node* root = new Node();
+    root->tag = "div";
+    root->text = "Hello Engine";
+
+    return root;
+}
